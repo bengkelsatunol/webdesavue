923583d6bc81be9159d100256cddf604a461423f
bengkel satunol <bengkelsatunol@gmail.com>
2025-03-28 23:26:36 +0900

    terbang

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import AboutView from './components/AboutView.vue'
 import HomeView from './components/HomeView.vue'
+import ServiceView from './components/ServiceView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -16,6 +17,11 @@ const router = createRouter({
       path: '/about',
       name: 'About',
       component: AboutView
+    },
+    {
+      path: '/services',
+      name: 'Services',
+      component: ServiceView
     }
   ]
 })
